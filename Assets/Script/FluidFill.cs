d57254ba3fa40d4c9a4c1fbbee85da113e103d2c
Miracle393 <100679386+Miracle393@users.noreply.github.com>
2024-05-02 19:41:18 +0300

    Test of scripting [PASS] now is to implement
    here is to create 3 buttons of titretion that has script on click it reduce 1 to 0.7, 0.7 to 0.6, 0.6 to 0.5 for the 1st to last button respectively.
    
    and the conical flask of soln B when filling other CF same way.
    
    and there is change in script jst niw for mesh render 1 for
    B nad MS2 for cf

--- Assets/Script/FluidFill.cs
+++ Assets/Script/FluidFill.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class FluidFill : MonoBehaviour
 {
-    public Material fluidMaterial;
+    public MeshRenderer meshRenderer1;
+    public MeshRenderer meshRenderer2;
+
+    //public Material fluidMaterial;
+
+    void Start()
+    {
+        meshRenderer1 = GetComponent<MeshRenderer>();
+        meshRenderer2 = GetComponent<MeshRenderer>();
+
+
+    }
 
     public void ReduceFluid()
     {
-        fluidMaterial.SetFloat("_Fill", 0.6f);
+        Debug.Log("Test Button");
+        meshRenderer1.material.SetFloat("_Fill", 0.7f);
+        meshRenderer2.material.SetFloat("_Fill", 0.5f);
     }
 }
